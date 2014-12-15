5c9307dac66c61c4150a29908566957b82fd6e4f
Malthe Milthers <malthe@milthers.dk>
2014-12-15 11:58:32 +0100

    add a 'add new' button to the calendar page

--- views/calendar-view.php
+++ views/calendar-view.php
@@ -1,4 +1,8 @@
 <div class="wrap">
-	<h2><?php _e('Calendar', 'mico-calendar'); ?></h2>
+	<h2>
+		<?php _e('Calendar', 'mico-calendar'); ?>
+		<?php $event_entry = get_post_type_object('event_entry' ); ?>
+		<a href="/wp-admin/post-new.php?post_type=event_entry" class="add-new-h2"><?php echo $event_entry->labels->add_new; ?></a>
+	</h2>
 	<div id="calendar" class="mcal-calendar-view"></div>
 </div> <!-- .wrap -->
